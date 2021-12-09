a1fb9420b8678dd205a53aa11d1217dddc685478
amro.abouelenein <amro.abouelenein@achieveinternet.com>
2021-12-09 17:29:49 +0100

    intial code repo update

---
+++ react/frontend/src/components/Header.js
@@ -0,0 +1,6 @@
+import React from "react";
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import CssBaseline  from "@material-ui/core/CssBaseline";
+import { makeStyles } from "@material-ui/core/styles";
--- ui_element_detection/ui_element_detection/settings.py
+++ ui_element_detection/ui_element_detection/settings.py
@@ -124,6 +124,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
